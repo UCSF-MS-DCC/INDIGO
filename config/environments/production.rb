d012a9eb5e7ce8ab319bcb87be2663869bc5d5c9
urrik98 <adamrenschen@gmail.com>
2016-12-06 09:45:13 -0800

    [chore] added actionmailer configurations to config/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,18 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'indigoprojectwebmaster@gmail.com',
+    password:             'hlakirucsf',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'warm-badlands-90928.herokuapp.com' }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
