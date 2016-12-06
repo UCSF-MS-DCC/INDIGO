fdb4f9d3225cf443e8567b8db3705d9c993ac113
urrik98 <adamrenschen@gmail.com>
2016-12-06 11:25:55 -0800

    [fix] deleted admin_contacts migration

--- db/migrate/20161104233044_add_subject_and_body_and_from_to_admin_contact.rb
+++
@@ -1,7 +0,0 @@
-class AddSubjectAndBodyAndFromToAdminContact < ActiveRecord::Migration[5.0]
-  def change
-    add_column :admin_contacts, :subject, :text
-    add_column :admin_contacts, :body, :text
-    add_column :admin_contacts, :from, :text
-  end
-end
