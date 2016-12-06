052d95942cb12dc6b1a8b806c3f707aecc134f14
urrik98 <adamrenschen@gmail.com>
2016-12-06 11:05:16 -0800

    [fix] slight change to user model, moved rolify to top

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  acts_as_token_authenticatable
   rolify
+  
+  acts_as_token_authenticatable
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -29,7 +31,17 @@ class User < ApplicationRecord
   def assign_default_role
     add_role(:user)
   end
-  
+
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
+
    def select_affiliations
      return ["Aberdeen University",
        "Guthy-Jackson Foundation",
