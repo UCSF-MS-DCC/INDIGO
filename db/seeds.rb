cc5a36793f38f0ba36294d388219754304446872
urrik98 <adamrenschen@gmail.com>
2016-12-09 10:24:28 -0800

    [fix] corrected text in new user sign up emails. [feat] added admin-enabled account to db.seed file to facilitate site administration at launch. [chore] lightened background of public facing pages.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(email: 'adam.renschen@ucsf.edu', password: '321321', password_confirmation: '321321', affiliation: 'UCSF')
+user = User.create(email: 'indigoprojectwebmaster@gmail.com', password: '321321', password_confirmation: '321321', affiliation: 'UCSF')
 user.update_attributes(:approved => true)
 user.add_role "admin"
