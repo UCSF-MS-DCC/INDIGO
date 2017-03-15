0b39099c83ed5b87a2f8cc2c100bda4a57e14716
urrik98 <adamrenschen@gmail.com>
2017-03-15 16:41:07 -0700

    Fix idr table name in migration file preventing deployment

--- db/migrate/20170315171644_add_age_of_onset_to_samples_and_id_rs.rb
+++ db/migrate/20170315171644_add_age_of_onset_to_samples_and_id_rs.rb
@@ -1,6 +1,6 @@
 class AddAgeOfOnsetToSamplesAndIdRs < ActiveRecord::Migration[5.0]
   def change
     add_column :samples, :age_of_onset, :integer
-    add_column :IDRs, :age_of_onset, :integer
+    add_column :idrs, :age_of_onset, :integer
   end
 end
