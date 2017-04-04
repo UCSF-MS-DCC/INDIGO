81bcca42a035c491a4fce97bc353ee4e7a69f145
urrik98 <adamrenschen@gmail.com>
2017-04-04 13:38:59 -0700

    clean up details in welcome/dataset and layout/footer views.

--- app/controllers/api/v1/apitests_controller.rb
+++ app/controllers/api/v1/apitests_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ApitestsController < ApplicationController
     @user = User.where(authentication_token: params[:user_token])
     @idr = IDR.where(sample_source: @user[0].affiliation).limit(10)
     if @idr.count == 0
-      render json: {'message':'there is no data from your institution available'}
+      render json: {'message':'there is no data from your institution available', 'request parameters':params }
     else
       render json: @idr
     end
