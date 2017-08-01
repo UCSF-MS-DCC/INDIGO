90a847ea8913b9d505b785e057519b9509395f32
urrik98 <adamrenschen@gmail.com>
2017-08-01 11:25:12 -0700

    Quick fix to samples download button controller action and sample model to_csv method.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -9,7 +9,7 @@ class Sample < ApplicationRecord
 
   def self.to_csv
     CSV.generate do |csv|
-      cols = %w(indigo_id site_sample_id disease received_date sent_date batch sex ethnicity site)
+      cols = %w(indigo_id sample_source_identifier disease received_date sent_date gender ethnicity)
       csv << cols
       all.each do |record|
         csv << record.attributes.values_at(*cols)
