26e5f185a04ec6a7d302ddf3223a346d0eb2becb
urrik98 <adamrenschen@gmail.com>
2018-12-03 15:34:55 -0800

    Slight changes to data progress table and processing progress chart. Add ALS to progress chart.

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -14,7 +14,7 @@ class ChartController < ApplicationController
   end
 
   def index_panel_two_data
-    diags = ["PD", "MG", "NMO"]
+    diags = ["PD", "MG", "NMO", "ALS"]
     data = []
     @samples = Sample.where(ngs_dataset:["HLA", "HLA and KIR", "HLA only"]).where.not(disease:["Unknown", "Not MS - Unaffected - Related", "Not MS - unaffected - related"])
     diags.each do |d|
@@ -29,21 +29,21 @@ class ChartController < ApplicationController
   end
 
   def index_panel_three_data
-    diags = ["PD", "MG", "NMO"]
+    diags = ["PD", "MG", "ALS"]
     data = []
     @samples = Sample.where(ngs_dataset:["HLA and KIR", "KIR only"]).where.not(disease:["Unknown", "Not MS - Unaffected - Related", "Not MS - unaffected - related"])
     diags.each do |d|
       data.push({:diagnosis => d,:kir_genotypes_available => @samples.where(disease:d).where(kir_raw:true).where(kir_geno:true).count,
                  :raw_kir_data_untyped => @samples.where(disease:d).where(kir_raw:true).where(kir_geno:false).count,
-                 :no_raw_kir_data =>   @samples.where(disease:d).where(kir_raw:false).where(kir_geno:false).count })
+                 :raw_kir_data_unavailable =>   @samples.where(disease:d).where(kir_raw:false).where(kir_geno:false).count })
     end
     # MS patients and controls have multiple values for diagnosis, making special query necessary
     data.push({:diagnosis => "MS",:kir_genotypes_available => @samples.where(disease:["MS", "MS - Confirmed", "MS - reported"]).where(kir_raw:true).where(kir_geno:true).count,
                :raw_kir_data_untyped => @samples.where(disease:["MS", "MS - Confirmed", "MS - reported"]).where(kir_raw:true).where(kir_geno:false).count,
-               :no_raw_kir_data =>   @samples.where(disease:["MS", "MS - Confirmed", "MS - reported"]).where(kir_raw:false).where(kir_geno:false).count })
+               :raw_kir_data_unavailable =>   @samples.where(disease:["MS", "MS - Confirmed", "MS - reported"]).where(kir_raw:false).where(kir_geno:false).count })
     data.push({:diagnosis => "Control",:kir_genotypes_available => @samples.where(disease:["HC", "Not MS - Unaffected - Unrelated - Spouse", "Not MS"]).where(kir_raw:true).where(kir_geno:true).count,
                :raw_kir_data_untyped => @samples.where(disease:["HC", "Not MS - Unaffected - Unrelated - Spouse", "Not MS"]).where(kir_raw:true).where(kir_geno:false).count,
-               :no_raw_kir_data =>   @samples.where(disease:["HC", "Not MS - Unaffected - Unrelated - Spouse", "Not MS"]).where(kir_raw:false).where(kir_geno:false).count })
+               :raw_kir_data_unavailable =>   @samples.where(disease:["HC", "Not MS - Unaffected - Unrelated - Spouse", "Not MS"]).where(kir_raw:false).where(kir_geno:false).count })
     render json: data, status: :ok
   end
 end
