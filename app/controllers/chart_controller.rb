7e86c50018d98c51fcb473180da418406f2faf93
urrik98 <adamrenschen@gmail.com>
2018-12-04 17:09:12 -0800

    ...

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -58,11 +58,11 @@ class ChartController < ApplicationController
     end
     loci.each do |locus|
       #data[locus.split("R")[1]] = Kir.where.not(locus.to_sym => nil).count
-      data.push({:locus => locus.split("R")[1], :Parkinsons_Disease => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease == ?","PD").count,
-                 :Multiple_Sclerosis => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease == ? OR ? OR ?","MS", "MS - Confirmed", "MS - reported").count,
-                 :Myasthenia_Gravis => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease == ?","MG").count,
-                 :Amyotrophic_Lateral_Sclerosis => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease == ?","ALS").count,
-                 :Control => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease == ?","HC").count
+      data.push({:locus => locus.split("R")[1], :Parkinsons_Disease => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease = ?","PD").count,
+                 :Multiple_Sclerosis => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease = ? OR ? OR ?","MS", "MS - Confirmed", "MS - reported").count,
+                 :Myasthenia_Gravis => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease = ?","MG").count,
+                 :Amyotrophic_Lateral_Sclerosis => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease = ?","ALS").count,
+                 :Control => Kir.joins(:sample).where.not(locus.to_sym => nil).where("samples.disease = ?","HC").count
       });
     end
 
