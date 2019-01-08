45656325c625422a07bf9ccb4d0d6c972156a928
urrik98 <adamrenschen@gmail.com>
2019-01-08 14:55:59 -0800

    Update to dataview graphs, revamp race chart as a kind of bar graph.

--- app/assets/javascripts/dataview.js
+++ app/assets/javascripts/dataview.js
@@ -457,123 +457,211 @@ $(document).on("turbolinks:load", function() {
     }
 
   });
-  // DATAVIEW/SUPERINDEX RACE CHART
-  // only do run when the user is accessing the superindex charts
+
   if(window.location.pathname == '/dataview/superindex') {
       $.get('/dataview/superindex_race_graph.json'+'?disease[]='+superindex_disease, function(data) {
-
-          var margin = {left:25, right:25, top:25, bottom:25}
-          var width = 350 - margin.left - margin.right;
+          console.log("DATA:", data);
+          var margin = {left:0, right:25, top:0, bottom:25}
+          var width = 475 - margin.left - margin.right;
           var height = 350 - margin.top - margin.bottom;
 
-          var radius = Math.min(width, height) / 2 - 50;
           var g = d3.select("#race-graph")
+              //.attr("style","border: 1px solid red")
               .append("svg")
               .attr("width", width)
               .attr("height", height)
               //.attr("style", "border:1px solid green")
+              .attr("transform", "translate(" + (margin.left) + "," + (margin.top) + ")")
               .append("g")
-              .attr("transform", "translate(" + (radius + margin.left * 4) + "," + (height / 1.8) + ")");
+              .attr("transform", "translate(" + 150 +",50)");
 
-          var colors = d3.scaleOrdinal()
-              .domain(data.map(function(d) { return d.race } ))
-              .range(["#333399", "#6a58b1","#9880ca","#ad95d7","#c2abe4","#d7c1f1","#ebd8ff"]);
 
-          var arc = d3.arc()
-              .innerRadius(0)
-              .outerRadius(radius);
+          var x = d3.scaleLinear()
+              .domain([0, d3.max(data, function(d) { return d.samples })])
+              .range([0, width/2 + 50]);
 
-          var pie = d3.pie()
-              .sort(null)
-              .value(function (d) {
-                  return d.samples;
-              });
+          var y = d3.scaleBand()
+              .domain(data.map(function(d) { return d.race }))
+              .range([0,height - 100])
+              .paddingInner(1.0)
+              .paddingOuter(0.3)
 
-          var pie = g.selectAll(".arc")
-              .data(pie(data));
-
-          pie.enter().append("path")
-              .attr("class", "arc")
-              .attr("d", arc)
-              .style("fill", function (d, i) {
-                  return colors(d.data.race);
-              })
-              .attr("stroke", "white")
-              .attr("stroke-width", "1px")
-              .on("mouseover", function(d) {
-                  tooltip.style("display", null);
-                  $('#legend-race-'+ removeSpace(d.data.race)).addClass("underlineText");})
-              .on("mouseout", function(d) {
-                  tooltip.style("display", "none");
-                  $('#legend-race-'+ removeSpace(d.data.race)).removeClass("underlineText");})
-              .on("mousemove", function(d) {
-                  var xPosition = d3.mouse(this)[0] - 30;
-                  var yPosition = d3.mouse(this)[1] - 45;
-                  tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-                  tooltip.select("text").text(d.data.samples);
-              });
-
-          // legend
-
-          var legend = g.append("g")
-              .attr("font-size", "12px")
-              .attr("text-anchor", "start")
+          var groups = g.append("g")
               .selectAll("g")
-              .data(data.map(function(d) { return d.race }))
+              .data(data)
               .enter().append("g")
-              .attr("transform", function(d, i) {
-                  return "translate("+ -450 + ","+ ((i * 18) - 125) + ")";
-              });
 
-          legend.append("rect")
-              .attr("x", width - 50)
-              .attr("width", 12)
-              .attr("height", 12)
-              .attr("fill", function(d, i) {
-                  return colors(d);
-              });
 
-          legend.append("text")
-              .attr("x", width - 30)
-              .attr("y", 12)
-              .attr("id", function(d) { return "legend-race-" + removeSpace(d); })
-              .text(function(d) {
-                  return capitalize(d);
-              })
-              .attr("font-size", "14px");
+          groups.selectAll("line")
+              .data(data)
+              .enter().append("line")
+              .attr("x1", 0)
+              .attr("x2", function(d) { return +x(d.samples) })
+              .attr("y1", function(d, i) {  return y(d.race) })
+              .attr("y2", function(d, i) { return +(y(d.race)) })
+              .style("stroke", "black")
+              .style("stroke-width", "1")
+              .style("stroke-dasharray", "3,6")
 
-          // tooltip
 
-          var tooltip = g.append("g")
-              .attr("class", "graph-tooltip")
-              .style("display", "none");
+          groups.selectAll("circle")
+              .data(data)
+              .enter().append("circle")
+              .attr("r", 5)
+              .attr("fill", 'black')
+              .attr("cx", function(d) { return +x(d.samples) - 4 })
+              .attr("cy", function(d) { return +(y(d.race)) })
 
-          tooltip.append("rect")
-              .attr("width", 45)
-              .attr("height", 30)
-              .attr("fill", "black");
 
-          tooltip.append("text")
-              .attr("x", 22.5)
-              .attr("dy", "1.2em")
-              .style("text-anchor", "middle")
-              .attr("font-size", "16px")
-              .attr("font-weight", "bold")
-              .attr("fill", 'white');
+          var xAxisCall = d3.axisBottom(x).ticks(4);
+          g.append("g")
+              .attr("class", "x-axis")
+              .attr("transform", "translate(0,"+ (height - 100) + ")")
+              .call(xAxisCall)
+              .selectAll("text")
+              .attr("font-size", "14px")
+              //.attr("transform", "rotate(-45)")
+              .attr("text-anchor", "middle")
+              //.attr("x","-7")
+              .attr("y", "10")
+
+          var yAxisCall = d3.axisLeft(y);
+          g.append("g")
+              .attr("class", "y-axis")
+              .attr("transform", "translate(0,0)")
+              .call(yAxisCall)
+              .selectAll("text")
+              .attr("font-size", "14px")
 
           // title
 
           g.append("text")
               .attr("class", "race-chart-title")
-              .attr("x", -50)
-              .attr("y", -150)
+              .attr("x", 100)
+              .attr("y", -32)
               .attr("font-size", "20px")
               .attr("text-anchor", "middle")
-              .text("Ancestry");
+              .text("Samples Received");
 
-
-      }); //closes GET dataview/superindex_race_graph callback
-  } //closes if window location == dataview/superindex
+      }); // close AJAX get callback
+  } // close if window.pathname == dataview/superindex block
+  // DATAVIEW/SUPERINDEX RACE PIE CHART
+  // only do run when the user is accessing the superindex charts
+  // if(window.location.pathname == '/dataview/superindex') {
+  //     $.get('/dataview/superindex_race_graph.json'+'?disease[]='+superindex_disease, function(data) {
+  //
+  //         var margin = {left:25, right:25, top:25, bottom:25}
+  //         var width = 350 - margin.left - margin.right;
+  //         var height = 350 - margin.top - margin.bottom;
+  //
+  //         var radius = Math.min(width, height) / 2 - 50;
+  //         var g = d3.select("#race-graph")
+  //             .append("svg")
+  //             .attr("width", width)
+  //             .attr("height", height)
+  //             //.attr("style", "border:1px solid green")
+  //             .append("g")
+  //             .attr("transform", "translate(" + (radius + margin.left * 4) + "," + (height / 1.8) + ")");
+  //
+  //         var colors = d3.scaleOrdinal()
+  //             .domain(data.map(function(d) { return d.race } ))
+  //             .range(["#333399", "#6a58b1","#9880ca","#ad95d7","#c2abe4","#d7c1f1","#ebd8ff"]);
+  //
+  //         var arc = d3.arc()
+  //             .innerRadius(0)
+  //             .outerRadius(radius);
+  //
+  //         var pie = d3.pie()
+  //             .sort(null)
+  //             .value(function (d) {
+  //                 return d.samples;
+  //             });
+  //
+  //         var pie = g.selectAll(".arc")
+  //             .data(pie(data));
+  //
+  //         pie.enter().append("path")
+  //             .attr("class", "arc")
+  //             .attr("d", arc)
+  //             .style("fill", function (d, i) {
+  //                 return colors(d.data.race);
+  //             })
+  //             .attr("stroke", "white")
+  //             .attr("stroke-width", "1px")
+  //             .on("mouseover", function(d) {
+  //                 tooltip.style("display", null);
+  //                 $('#legend-race-'+ removeSpace(d.data.race)).addClass("underlineText");})
+  //             .on("mouseout", function(d) {
+  //                 tooltip.style("display", "none");
+  //                 $('#legend-race-'+ removeSpace(d.data.race)).removeClass("underlineText");})
+  //             .on("mousemove", function(d) {
+  //                 var xPosition = d3.mouse(this)[0] - 30;
+  //                 var yPosition = d3.mouse(this)[1] - 45;
+  //                 tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+  //                 tooltip.select("text").text(d.data.samples);
+  //             });
+  //
+  //         // legend
+  //
+  //         var legend = g.append("g")
+  //             .attr("font-size", "12px")
+  //             .attr("text-anchor", "start")
+  //             .selectAll("g")
+  //             .data(data.map(function(d) { return d.race }))
+  //             .enter().append("g")
+  //             .attr("transform", function(d, i) {
+  //                 return "translate("+ -450 + ","+ ((i * 18) - 125) + ")";
+  //             });
+  //
+  //         legend.append("rect")
+  //             .attr("x", width - 50)
+  //             .attr("width", 12)
+  //             .attr("height", 12)
+  //             .attr("fill", function(d, i) {
+  //                 return colors(d);
+  //             });
+  //
+  //         legend.append("text")
+  //             .attr("x", width - 30)
+  //             .attr("y", 12)
+  //             .attr("id", function(d) { return "legend-race-" + removeSpace(d); })
+  //             .text(function(d) {
+  //                 return capitalize(d);
+  //             })
+  //             .attr("font-size", "14px");
+  //
+  //         // tooltip
+  //
+  //         var tooltip = g.append("g")
+  //             .attr("class", "graph-tooltip")
+  //             .style("display", "none");
+  //
+  //         tooltip.append("rect")
+  //             .attr("width", 45)
+  //             .attr("height", 30)
+  //             .attr("fill", "black");
+  //
+  //         tooltip.append("text")
+  //             .attr("x", 22.5)
+  //             .attr("dy", "1.2em")
+  //             .style("text-anchor", "middle")
+  //             .attr("font-size", "16px")
+  //             .attr("font-weight", "bold")
+  //             .attr("fill", 'white');
+  //
+  //         // title
+  //
+  //         g.append("text")
+  //             .attr("class", "race-chart-title")
+  //             .attr("x", -50)
+  //             .attr("y", -150)
+  //             .attr("font-size", "20px")
+  //             .attr("text-anchor", "middle")
+  //             .text("Ancestry");
+  //
+  //
+  //     }); //closes GET dataview/superindex_race_graph callback
+  // } //closes if window location == dataview/superindex
 
     // DATAVIEW/SUPERINDEX SEX CHART
     if(window.location.pathname == '/dataview/superindex') {
@@ -585,6 +673,7 @@ $(document).on("turbolinks:load", function() {
 
             var radius = Math.min(width, height) / 2 - 50;
             var g = d3.select("#sex-graph")
+                //.attr("style","border: 1px solid red")
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height)
