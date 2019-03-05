fc11d8324cb2bf6aa4f6acd6576dc16817585281
urrik98 <adamrenschen@gmail.com>
2019-03-05 14:53:16 -0800

    ...

--- db/migrate/20190305005050_add_notes_to_samples.rb
+++ db/migrate/20190305005050_add_notes_to_samples.rb
@@ -1,6 +1,6 @@
 class AddNotesToSamples < ActiveRecord::Migration[5.0]
   def change
-    add_column :samples, :notes, :text
+    add_column :samples, :notes, :string
     add_column :samples, :exclude, :boolean
   end
 end
